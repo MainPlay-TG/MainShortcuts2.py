0ce05d7ccdd42de4d6bd546e76f0ae1e85ae82c7
MainPlay-YT <xbox.roman6666666666@gmail.com>
2024-08-26 00:35:23 +0700

    no-comment

--- .manager/after_upload.py
+++ .manager/after_upload.py
@@ -7,16 +7,18 @@ def run(*args,**kw)->subprocess.Popen:
   p=subprocess.Popen(**kw)
   p.wait()
   return p
+print("Загрузка релиза на GitHub")
 with open("pyproject.toml","rb") as f:
   proj=tomli.load(f)
+name=proj["tool"]["poetry"]["name"]
 version=proj["tool"]["poetry"]["version"]
 args=["gh","release","create"]
 args.append("v%s"%version)
-args+=["-t","MainShortcuts %s"%version]
+args+=["-t","%s %s"%(name,version)]
 args+=["-F","changelog/%s.md"%version]
-for i in ms.dir.list("dist",func=lambda p:p.full_name.lower().startswith("mainshortcuts2-%s"%version)):
+for i in ms.dir.list("dist",func=lambda p:p.full_name.lower().startswith("%s-%s"%(name.lower(),version))):
   if i.ext==".gz":
-    args.append(i.path+"#MainShortcuts2.tar.gz")
+    args.append(i.path+("#%s.tar.gz"%name))
   else:
     args.append(i.path)
 run(*args)
