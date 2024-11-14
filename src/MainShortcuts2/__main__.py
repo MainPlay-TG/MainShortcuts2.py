9cb2d947c117b499b1945bac4dcb4f5b147e58c4
MainPlay-YT <xbox.roman6666666666@gmail.com>
2024-11-14 20:53:18 +0700

    no-comment

--- src/MainShortcuts2/__main__.py
+++ src/MainShortcuts2/__main__.py
@@ -16,21 +16,21 @@ def nano_json():
   import subprocess
   argp = argparse.ArgumentParser("nano-json", description="форматирование JSON файлов и редактирование в GNU NANO")
   argp.add_argument("files", nargs="+", help="пути к файлам JSON")
-  argp.add_argument("--encoding", default="utf-8", help="кодировка файлов"
+  argp.add_argument("--encoding", default="utf-8", help="кодировка файлов")
   argp.add_argument("--nano-help", action="store_true", help="показать помощь nano")
   argp.add_argument("--no-escape", action="store_false", help="не использовать Unicode Escape")
   argp.add_argument("-f", "--rcfile", help="использовать только этот файл для настройки nano")
   argp.add_argument("-m", "--mode", choices=ms.json.MODES, default="p", help="режим сохранения редактирования")
-  args=argp.parse_args()
+  args = argp.parse_args()
   if args.nano_help:
     return subprocess.call(["nano", "--help"])
-  nano_args=["nano"]
+  nano_args = ["nano"]
   if args.rcfile:
     nano_args += ["--rcfile", args.rcfile]
   nano_args += args.files
   for i in args.files:
     try:
-      data=ms.json.read(i, encoding=args.encoding)
+      data = ms.json.read(i, encoding=args.encoding)
       ms.json.write(i + ms.file.TMP_SUFFIX, data, encoding=args.encoding, ensure_ascii=False, mode="p")
       ms.file.delete(i)
       ms.file.move(i + ms.file.TMP_SUFFIX, i)
@@ -39,7 +39,7 @@ def nano_json():
   subprocess.call(nano_args)
   for i in args.files:
     try:
-      data=ms.json.read(i, encoding=args.encoding)
+      data = ms.json.read(i, encoding=args.encoding)
       ms.json.write(i + ms.file.TMP_SUFFIX, data, encoding=args.encoding, ensure_ascii=args.no_escape, mode=args.mode)
       ms.file.delete(i)
       ms.file.move(i + ms.file.TMP_SUFFIX, i)
@@ -50,7 +50,7 @@ def nano_json():
 def _check_nginx() -> int:
   import subprocess
   with subprocess.Popen(["nginx", "-t"], stderr=subprocess.PIPE) as p:
-    code=p.wait()
+    code = p.wait()
     if code != 0:
       sys.stderr.buffer.write(p.stderr.read())
   return code
@@ -59,9 +59,9 @@ def _check_nginx() -> int:
 def nginx_reload():
   ms.utils.check_programs("nginx")
   import subprocess
-  argp=argparse.ArgumentParser("nginx-reload", description="проверка конфига и перезагрузка Nginx")
+  argp = argparse.ArgumentParser("nginx-reload", description="проверка конфига и перезагрузка Nginx")
   argp.parse_args()
-  code=_check_nginx()
+  code = _check_nginx()
   if code != 0:
     sys.exit(code)
   sys.exit(subprocess.call(["nginx", "-s", "reload"]))
@@ -70,9 +70,9 @@ def nginx_reload():
 def nginx_restart():
   ms.utils.check_programs("nginx", "systemctl")
   import subprocess
-  argp=argparse.ArgumentParser("nginx-reload", description="проверка конфига и перезапуск Nginx через systemctl")
+  argp = argparse.ArgumentParser("nginx-reload", description="проверка конфига и перезапуск Nginx через systemctl")
   argp.parse_args()
-  code=_check_nginx()
+  code = _check_nginx()
   if code != 0:
     sys.exit(code)
   sys.exit(subprocess.call(["systemctl", "restart", "nginx"]))
@@ -81,36 +81,36 @@ def nginx_restart():
 def hash_gen():
   import os
   import hashlib
-  argp=argparse.ArgumentParser("ms2-hash-gen", description="создание контрольной суммы для файла")
+  argp = argparse.ArgumentParser("ms2-hash-gen", description="создание контрольной суммы для файла")
   argp.add_argument("files", nargs="+", help="пути к файлам")
   argp.add_argument("-b", "--bar", action="store_true", help="показывать прогрессбар (нужен модуль progressbar2)")
   argp.add_argument("-t", "--type", choices=HASH_TYPES, default="sha512", help="тип контрольной суммы")
-  args=argp.parse_args()
+  args = argp.parse_args()
   if args.bar:
     import progressbar
-    pbar_w=[
-      progressbar.Percentage(),
-      progressbar.GranularBar(left="(", right=")"),
-      progressbar.FileTransferSpeed(),
-      progressbar.ETA(format="%(eta)8s", format_finished="%(elapsed)8s", format_na="     N/A", format_not_started="--:--:--", format_zero="00:00:00"),
+    pbar_w = [
+        progressbar.Percentage(),
+        progressbar.GranularBar(left="(", right=")"),
+        progressbar.FileTransferSpeed(),
+        progressbar.ETA(format="%(eta)8s", format_finished="%(elapsed)8s", format_na="     N/A", format_not_started="--:--:--", format_zero="00:00:00"),
     ]
-  data={}
-  data["format"]="MainShortcuts2_hash_v1"
-  data["file"]={}
-  data["hash"]={}
-  data["hash"]["type"]=args.type
-  Hash=getattr(hashlib, args.type)
+  data = {}
+  data["format"] = "MainShortcuts2_hash_v1"
+  data["file"] = {}
+  data["hash"] = {}
+  data["hash"]["type"] = args.type
+  Hash = getattr(hashlib, args.type)
   for file in args.files:
-    hash=Hash()
-    data["file"]["size"]=os.path.getsize(file)
+    hash = Hash()
+    data["file"]["size"] = os.path.getsize(file)
     if args.bar:
-      pbar=progressbar.ProgressBar(
-        max_error=False,
-        max_value=data["file"]["size"],
-        min_value=0,
-        widgets=[ms.path.Path(file).full_name] + pbar_w,
+      pbar = progressbar.ProgressBar(
+          max_error=False,
+          max_value=data["file"]["size"],
+          min_value=0,
+          widgets=[ms.path.Path(file).full_name] + pbar_w,
       )
-      c=0
+      c = 0
       pbar.start()
     with open(file, "rb") as f:
       for chunk in f:
@@ -120,43 +120,45 @@ def hash_gen():
           pbar.update(c)
     if args.bar:
       pbar.finish(dirty=True)
-    data["hash"]["hex"]=hash.hexdigest()
+    data["hash"]["hex"] = hash.hexdigest()
     ms.json.write(file + HASH_SUFFIX, data)
+
+
 def hash_check():
   import hashlib
   import os
   import shlex
-  argp=argparse.ArgumentParser("ms2-hash-check", description="проверка размера и контрольной суммы файла")
+  argp = argparse.ArgumentParser("ms2-hash-check", description="проверка размера и контрольной суммы файла")
   argp.add_argument("-b", "--bar", action="store_true", help="показывать прогрессбар (нужен модуль progressbar2)")
   argp.add_argument("files", nargs="+", help="пути к файлам")
-  args=argp.parse_args()
+  args = argp.parse_args()
   if args.bar:
     import progressbar
-    pbar_w=[
-      progressbar.Percentage(),
-      progressbar.GranularBar(left="(", right=")"),
-      progressbar.FileTransferSpeed(),
-      progressbar.ETA(format="%(eta)8s", format_finished="%(elapsed)8s", format_na="     N/A", format_not_started="--:--:--", format_zero="00:00:00"),
+    pbar_w = [
+        progressbar.Percentage(),
+        progressbar.GranularBar(left="(", right=")"),
+        progressbar.FileTransferSpeed(),
+        progressbar.ETA(format="%(eta)8s", format_finished="%(elapsed)8s", format_na="     N/A", format_not_started="--:--:--", format_zero="00:00:00"),
     ]
   for file in args.files:
     if file.lower().endswith(HASH_SUFFIX.lower()):
-      file=file[:0 - len(HASH_SUFFIX)]
+      file = file[:0 - len(HASH_SUFFIX)]
     if not os.path.exists(file + HASH_SUFFIX):
       print("Ошибка: не найден файл " + shlex.quote(file + HASH_SUFFIX), file=sys.stderr)
       continue
-    data=ms.json.read(file + HASH_SUFFIX)
+    data = ms.json.read(file + HASH_SUFFIX)
     if data["file"]["size"] != os.path.getsize(file):
       print("Ошибка: размер файла " + shlex.quote(file) + " не совпадает")
       continue
-    hash=getattr(hashlib, data["hash"]["type"])()
+    hash = getattr(hashlib, data["hash"]["type"])()
     if args.bar:
-      pbar=progressbar.ProgressBar(
-        max_error=False,
-        max_value=data["file"]["size"],
-        min_value=0,
-        widgets=[ms.path.Path(file).full_name] + pbar_w,
+      pbar = progressbar.ProgressBar(
+          max_error=False,
+          max_value=data["file"]["size"],
+          min_value=0,
+          widgets=[ms.path.Path(file).full_name] + pbar_w,
       )
-      c=0
+      c = 0
       pbar.start()
     with open(file, "rb") as f:
       for chunk in f:
--- src/MainShortcuts2/_module_info.py
+++ src/MainShortcuts2/_module_info.py
@@ -1,2 +1,2 @@
 name = "MainShortcuts2"
-version = "2.4.0"
+version = "2.4.1"
